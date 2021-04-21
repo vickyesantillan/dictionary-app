ed3164bd0140967f1266b4d90accaddf638deedc
vickyesantillan <vesantillan@live.com>
2021-04-21 10:15:30 -0300

    Upgrading the convert program.

--- kg_to_pounds.py
+++ kg_to_pounds.py
@@ -1,42 +1,48 @@
 # Practice of tkinter library
 
+
 from tkinter import *
 
 windows = Tk()
 
+# Convert Function
+
 
 def convert():
+
     pounds = float(kg_value.get()) * 2.20462
-    print('pounds:', pounds)
     pounds1.insert(END, pounds)
 
     grams = float(kg_value.get()) * 1000
-    print('grams: ', grams)
     grams1.insert(END, grams)
 
     ounces = float(kg_value.get()) * 35.274
-    print('ounces:', ounces)
     ounces1.insert(END, ounces)
+
+# Convert botton
 
 
 convert = Button(windows, text="Convert", command=convert)
 convert.grid(row=0, column=2)
 
+# Kg entry value
 kg_value = StringVar()
 kg = Entry(windows, textvariable=kg_value)
 kg.grid(row=0, column=1)
 
+# Kg label
 Kg_label = Label(windows, text="Kg")
 Kg_label.grid(row=0, column=0)
 
+# Converts results
 
-grams1 = Text(windows, height=1, width=10)
+grams1 = Text(windows, height=1, width=20)
 grams1.grid(row=1, column=0)
 
-pounds1 = Text(windows, height=1, width=10)
+pounds1 = Text(windows, height=1, width=20)
 pounds1.grid(row=1, column=1)
 
-ounces1 = Text(windows, height=1, width=10)
+ounces1 = Text(windows, height=1, width=20)
 ounces1.grid(row=1, column=2)
 
 
