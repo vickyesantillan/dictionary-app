a5bb3e7d3968975ce2cf6e27b484167bbfe988da
vickyesantillan <vesantillan@live.com>
2021-05-15 09:25:33 -0300

    adding comentaries of the function

--- kg_to_pounds.py
+++ kg_to_pounds.py
@@ -13,8 +13,9 @@ windows = Tk()
 def convert():
 
     pounds1.delete("1.0", END)  # Deleting the old value
-    pounds = round(float(kg_value.get()) * 2.20462, 2)
-    pounds1.insert(END, pounds)
+    pounds = round(float(kg_value.get()) * 2.20462,
+                   2)  # calculating the values
+    pounds1.insert(END, pounds)  # Inserting the values in the text box
 
     grams1.delete("1.0", END)  # Deleting the old value
     grams = round(float(kg_value.get()) * 1000, 2)
@@ -31,9 +32,10 @@ convert = Button(windows, text="Convert", command=convert)
 convert.grid(row=0, column=2)
 
 # Kg entry value
-kg_value = StringVar()
+kg_value = StringVar()  # creates a value
+# creates a place to input the value
 kg = Entry(windows, textvariable=kg_value)
-kg.grid(row=0, column=1)
+kg.grid(row=0, column=1)  # position of the entry box
 
 # Kg label
 Kg_label = Label(windows, text="Kg➡")
